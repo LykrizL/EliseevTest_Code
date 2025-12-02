7b99c6221a219a3284fa59b8fcbccd9e0b4fb80e
Pavel <lykriz@vk.com>
2025-12-02 22:03:47 +0500

    Добавили переменную "с" и вывод суммы "a + b + c"

--- Program.cs
+++ Program.cs
@@ -12,9 +12,11 @@ namespace EliseevTest
         {
             int a = 5;
             int b = 10;
+            int c = 20;
             Console.WriteLine($"a = {a}");
             Console.WriteLine($"b = {b}");
             Console.WriteLine($"a + b = {a + b} ");
+            Console.WriteLine($"a + b + c = {a + b + c} ");
             Console.ReadKey();
         }
     }
